6d84aa355f62867a1e98c4fffff168b69718b77d
黒田一道 <>
2018-10-04 23:24:46 +0900

    Update component App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyHelloText from './atoms/Text/MyHelloText'
+import MyTypeDebug from './atoms/Debug/MyTypeDebug';
+import MyAttrText from './atoms/Text/MyAttrText';
+import MyHelloContentText from './atoms/Text/MyHelloContentText';
+import MyBook from './molecules/MyBooks/MyBooks';
 
-class App extends Component {
+class App extends Component 
+{
+  renderMyAttrText(){
+    const data = {
+      name:'山田権左衛門',
+      age: 18,
+      sex: '男'
+    };
+
+    return (
+      <MyAttrText {...data} />
+    )
+  }
+
+  renderMyBook() {
+    const book = {
+      isbn: 'WGS-JST-001',
+      title:'速習 webpack',
+      price:454,
+      published:'WINGSプロジェクト'
+    };
+
+    return <MyBook info={book} />
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,6 +49,19 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <div>
+          <MyHelloText name="山田" />
+          <MyTypeDebug value="山田" />
+          <MyTypeDebug value={ '鈴木' } />
+          <MyTypeDebug value={ 108 }  />
+          <MyTypeDebug value={ true } />
+          <MyTypeDebug value={ ['うめ', 'もも', 'さくら'] } />
+          <MyTypeDebug value={ {name: '山田太郎', age: 40} } />
+          <MyTypeDebug value={ () => console.log('Hoge') } />
+          {this.renderMyAttrText()}
+          <MyHelloContentText>山田</MyHelloContentText>
+          {this.renderMyBook()}
+        </div>
       </div>
     );
   }
