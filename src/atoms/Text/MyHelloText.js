506f997fbf473555608f29ffcf5e4aa53453def7
黒田一道 <>
2018-10-06 19:52:19 +0900

    MyHelloText 型、規定値の定義方法を変更

--- src/atoms/Text/MyHelloText.js
+++ src/atoms/Text/MyHelloText.js
@@ -3,21 +3,23 @@ import PropTypes from 'prop-types';
 
 export default class MyHelloText extends Component 
 {
+  /**
+   * 型定義
+   */
+  static propTypes = {
+    name: PropTypes.string.isRequired
+  }
+  
+  /**
+   * 規定値を定義
+   */
+  static defaultProps = {
+    name: '名無しの権兵衛'
+  };
+
+  /** 描画 */
   render() {
     return <div>こんにちは、{this.props.name}さん!</div>;
   }
 }
 
-/**
- * 型定義
- */
-MyHelloText.propTypes = {
-  name: PropTypes.string.isRequired
-};
-
-/**
- * 規定値を定義
- */
-MyHelloText.defaultProps = {
-  name: '名無しの権兵衛'
-};
