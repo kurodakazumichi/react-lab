0dc8791ba38af6ee9992a78dc6d9910fee36653e
黒田一道 <>
2018-10-06 19:30:53 +0900

    MyPropコンポーネント作成

---
+++ src/atoms/Debug/MyProp.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * メンバー
+ */
+export class Member{}
+
+export default class MyProp extends Component 
+{
+    /** 描画 */
+    render() 
+    {
+        console.log(this.props);
+        return <p>結果はコンソールを確認してください。</p>
+    }
+}
+
+MyProp.propTypes = {
+    prop1: PropTypes.instanceOf(Member)
+}
